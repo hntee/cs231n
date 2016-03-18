5edaa3b2dda8917073477c603e1bf64901954070
hntee <ihntee@gmail.com>
2016-03-18 15:11:44 +0800

    softmax and svm

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -4,59 +4,63 @@ from random import shuffle
 def softmax_loss_naive(W, X, y, reg):
   """
   Softmax loss function, naive implementation (with loops)
-
-  Inputs have dimension D, there are C classes, and we operate on minibatches
-  of N examples.
-
   Inputs:
-  - W: A numpy array of shape (D, C) containing weights.
-  - X: A numpy array of shape (N, D) containing a minibatch of data.
-  - y: A numpy array of shape (N,) containing training labels; y[i] = c means
-    that X[i] has label c, where 0 <= c < C.
+  - W: D x c array of weights
+  - X: N x D array of data. Data are D-dimensional columns
+  - y: 1-dimensional array of length N with labels 0...C-1, for C classes
   - reg: (float) regularization strength
-
-  Returns a tuple of:
+  Returns:
+  a tuple of:
   - loss as single float
-  - gradient with respect to weights W; an array of same shape as W
+  - gradient with respect to weights W, an array of same size as W
   """
   # Initialize the loss and gradient to zero.
-  loss = 0.0
+  data_loss = 0.0
   dW = np.zeros_like(W)
+  n_sample, n_feature  = X.shape
+  assert n_feature == W.shape[0]
+  for i in xrange(n_sample):
+    scores = np.dot(X[i],W)
+    scores -= np.max(scores)
+    scores = np.exp(scores)
+    probability = scores / np.sum(scores)
 
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using explicit loops.     #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-  pass
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
+    #accumulate the data_loss
+    sample_loss = -np.log(probability[y[i]])
+    data_loss += sample_loss / n_sample
+
+    #accumulate the gradient with respect to every single data sample
+    probability[y[i]] -= 1
+    dW_single_sample = np.outer(X[i],probability) / n_sample
+    dW += dW_single_sample
+
+  reg_loss = 0.5 * reg * np.sum(W[:-1,:] ** 2)
+  loss = data_loss + reg_loss
 
   return loss, dW
-
 
 def softmax_loss_vectorized(W, X, y, reg):
   """
   Softmax loss function, vectorized version.
-
   Inputs and outputs are the same as softmax_loss_naive.
   """
   # Initialize the loss and gradient to zero.
-  loss = 0.0
   dW = np.zeros_like(W)
+  n_sample = X.shape[0]
 
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-  pass
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
+  assert X.shape[1] == W.shape[0]
+  scores = np.dot(X,W)
+
+  scores -= np.max(scores, axis = 1, keepdims = True)
+  scores = np.exp(scores)
+  probs = scores / np.sum(scores, axis = 1, keepdims = True)
+  kl_loss = np.sum(-np.log(probs[range(n_sample),y])) / n_sample
+  reg_loss = 0.5 * reg * np.sum(W[:-1,:] ** 2)
+  loss = kl_loss + reg_loss
+
+  dscores = probs
+  dscores[range(n_sample),y] -= 1
+  dscores /= n_sample
+  dW = np.dot(X.T, dscores)
 
   return loss, dW
-
